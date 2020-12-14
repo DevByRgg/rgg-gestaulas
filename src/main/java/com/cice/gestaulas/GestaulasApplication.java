46080246e5aaec930086242371fe1c86d38943ec
rgilgil <>
2020-12-14 12:49:23 +0100

    Initial commit

---
+++ src/main/java/com/cice/gestaulas/GestaulasApplication.java
@@ -0,0 +1,13 @@
+package com.cice.gestaulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cice/gestaulas/GestaulasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cice.gestaulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
