310c4699e1d935e425184770b08f99443f4fc5ac
diegosg4646 <diegosg4646@gmail.com>
2020-12-25 21:24:52 +0100

    Puesto el calendario en castellano

--- src/main/webapp/view/public/app.js
+++ src/main/webapp/view/public/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $("#calendar").fullCalendar();
+    $('#calendar').fullCalendar({
+		monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+		monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+		dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+		dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb']
+	});
 });
