419385093cce9697e6ded30f335c638605219844
diegosg4646 <diegosg4646@gmail.com>
2020-12-26 15:44:06 +0100

    Agenda modificada app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,10 +3,23 @@ $(document).ready(function() {
 //Puesto en Castellano
 
 	$('#calendar').fullCalendar({
+		defaultView: 'agendaDay',
+		lang: 'es',
+		columnFormat: 'ddd',
 		monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
 		monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
-		dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-		dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb']
+		dayNames: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+		dayNamesShort: ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+		allDaySlot: false,
+		weekend: true,
+		minTime: '08:00:00',
+		maxTime: '15:00:00',
+		slotDuration: '01:00:00'
 	});
 
 
