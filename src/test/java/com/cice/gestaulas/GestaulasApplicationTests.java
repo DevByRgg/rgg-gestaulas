13e8184fb3f4909dcc1cd09c0f75d482f08404e5
diegosg4646 <diegosg4646@gmail.com>
2021-01-12 21:32:58 +0100

    Test para probar messagesproperties

--- src/test/java/com/cice/gestaulas/GestaulasApplicationTests.java
+++ src/test/java/com/cice/gestaulas/GestaulasApplicationTests.java
@@ -1,5 +1,17 @@
 package com.cice.gestaulas;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -31,9 +43,44 @@ class GestaulasApplicationTests {
 		
 	}
 	
+	@Test
+    public void testEmpty() {
+		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<Aula>> violations = validator.validate(new Aula());
+        assertEquals(2, violations.size());
+        violations.forEach(v -> System.out.println(
+                v.getPropertyPath() + " : " + v.getMessageTemplate() + " = " + v.getMessage()));
+    }
+	
 	
 	@Test
-	void contextLoads() {
+	void testValidationMessagesEs() {
+		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+		Locale.setDefault(new Locale("es", "ES"));
+	    ResourceBundle rb = ResourceBundle.getBundle("ValidationMessages_es");
+	    System.out.println("LEYENDO ValidationMessages.properties_ ----  " + rb.getString("aula.nombre.empty"));
+	    Set<ConstraintViolation<Aula>> violations = validator.validate(new Aula());
+	    for (ConstraintViolation<Aula> constraintViolation : violations) {
+	    	System.out.println("VIOLATIONS MESSAGE: ------------- " + constraintViolation.getMessage()); 
+		}
+	    assertFalse(violations.isEmpty(), ()->"No hay ningun error constraint");
+	    assertTrue(violations.stream().anyMatch(v->v.getMessage().equals(rb.getString("aula.nombre.empty"))));
 	}
+	
+	/*
+	 * @Test void testValidationMessagesEn() { Validator validator =
+	 * Validation.buildDefaultValidatorFactory().getValidator();
+	 * Locale.setDefault(new Locale("en", "EN")); ResourceBundle rb =
+	 * ResourceBundle.getBundle("ValidationMessages_en");
+	 * System.out.println("LEYENDO ValidationMessages.properties_ ----  " +
+	 * rb.getString("aula.nombre.empty")); Set<ConstraintViolation<Aula>> violations
+	 * = validator.validate(new Aula()); for (ConstraintViolation<Aula>
+	 * constraintViolation : violations) {
+	 * System.out.println("VIOLATIONS MESSAGE: ------------- " +
+	 * constraintViolation.getMessage()); } assertFalse(violations.isEmpty(),
+	 * ()->"No hay ningun error constraint");
+	 * assertTrue(violations.stream().anyMatch(v->v.getMessage().equals(rb.getString
+	 * ("aula.nombre.empty")))); }
+	 */
 
 }
