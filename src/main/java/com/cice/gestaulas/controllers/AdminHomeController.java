cd88632bfeb1acd02d5226d56250a0d51acb750f
rgilgil <>
2021-01-17 12:52:52 +0100

    rgg

--- src/main/java/com/cice/gestaulas/controllers/AdminHomeController.java
+++ src/main/java/com/cice/gestaulas/controllers/AdminHomeController.java
@@ -3,9 +3,6 @@ package com.cice.gestaulas.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
